d0ee3aade21f08823f0d684140c466bf21739cbe
upulera <upul.eranda@gmail.com>
2025-09-05 19:29:48 +0200

    exec.c final refactoring: need code formatting

--- executor/exec.c
+++ executor/exec.c
@@ -83,61 +83,85 @@ static int exec_command_builtins(t_shell *shell, t_command *cmd)
 	return (shell->exit_status);
 }
 
-static int	exec_command_externals(t_shell *shell, t_command *cmd)
-{
-	pid_t			pid;
-	int				status;
-	int				access_err;
+static void exec_command_in_child(t_shell *shell, t_command *cmd)
+{	
+	int		access_err;
 
-	pid = -1;
-	status = -1;
-
-	signal(SIGINT, SIG_IGN);
-	signal(SIGQUIT, SIG_IGN);
-	pid = fork();
-	if (pid == 0)
-	{
-		signal(SIGINT, SIG_DFL);
-		signal(SIGQUIT, SIG_DFL);
-		if (apply_redirs(shell, cmd->redirs, cmd->command_kind, CHILD_SHELL))
-			exit (1);
-		if (cmd->args)
-		{
-			access_err = ft_check_access(shell, cmd);
-			if (access_err)
-				exit (access_err);
-		}
-		if (shell->parse_err == 5)
-			exit (1);
-		if (!cmd->args)
-			exit(0);
-		execve((cmd->args)[0], cmd->args, shell->env->data);
-		perror("execve");
-		exit(127);
+	signal(SIGINT, SIG_DFL);
+	signal(SIGQUIT, SIG_DFL);
+	if (apply_redirs(shell, cmd->redirs, cmd->command_kind, CHILD_SHELL))
+		exit (1);
+	if (cmd->args)
+	{	
+		access_err = ft_check_access(shell, cmd);
+		if (access_err)
+			exit (access_err);
 	}
-	waitpid(pid, &status, 0);
-	signal(SIGINT, ft_sigint_handler);
-	signal(SIGQUIT, ft_sigquit_trap);
-	if (WIFSIGNALED(status))
+	if (shell->parse_err == 5)
+		exit (1);
+	if (!cmd->args)
+		exit(0);
+	execve((cmd->args)[0], cmd->args, shell->env->data);
+	perror("execve");
+	exit(127);
+}
+
+static int exec_command_signal_handle (int *status)
+{
+	int sig;
+
+	sig = -1;
+	if (WIFSIGNALED(*status))
 	{
-		int sig = WTERMSIG(status);
+		sig = WTERMSIG(*status);
 		if (sig == SIGINT)
 			write(STDOUT_FILENO, "\n", 1);
 		else if (sig == SIGQUIT)
 			write(STDOUT_FILENO, "Quit (core dumped)\n", 19);
-		return 128 + sig;
+		return (128 + sig);
 	}
-	if (WIFEXITED(status))
-		return (WEXITSTATUS(status));
+	if (WIFEXITED(*status))
+		return (WEXITSTATUS(*status));
+	return (sig);
+}
+
+static int	exec_command_externals(t_shell *shell, t_command *cmd)
+{
+	pid_t			pid;
+	int				status;
+	int				exitcode;
+
+	pid = -1;
+	status = -1;
+	exitcode = -1;
+	signal(SIGINT, SIG_IGN);
+	signal(SIGQUIT, SIG_IGN);
+	pid = fork();
+	if (pid == 0)
+		exec_command_in_child(shell, cmd);
+	waitpid(pid, &status, 0);
+	signal(SIGINT, ft_sigint_handler);
+	signal(SIGQUIT, ft_sigquit_trap);
+	
+	// if (WIFSIGNALED(status))
+	// {
+	// 	int sig = WTERMSIG(status);
+	// 	if (sig == SIGINT)
+	// 		write(STDOUT_FILENO, "\n", 1);
+	// 	else if (sig == SIGQUIT)
+	// 		write(STDOUT_FILENO, "Quit (core dumped)\n", 19);
+	// 	return (128 + sig);
+	// }
+	// if (WIFEXITED(status))
+	// 	return (WEXITSTATUS(status));
+	exitcode = exec_command_signal_handle(&status);
+	if (exitcode != -1)
+		return (exitcode);
 	return (1);
 }
 
 static	int	exec_command(t_shell *shell, t_command *cmd)
 {
-	// pid_t			pid;
-	// int				status;
-	// int				access_err;
-
 	if (cmd->args)
 	{
 		ft_skip_empty_vars(shell, cmd->args);
@@ -145,52 +169,10 @@ static	int	exec_command(t_shell *shell, t_command *cmd)
 			return (0);
 		exec_command_expansions(shell, cmd);
 	}
-	// pid = -1;
-	// status = -1;
 	if (cmd->command_kind == BUILTIN)
 		return (exec_command_builtins(shell, cmd));
 	else
-	{
-		// signal(SIGINT, SIG_IGN);
-		// signal(SIGQUIT, SIG_IGN);
-		// pid = fork();
-		// if (pid == 0)
-		// {
-		// 	signal(SIGINT, SIG_DFL);
-		// 	signal(SIGQUIT, SIG_DFL);
-		// 	if (apply_redirs(shell, cmd->redirs, cmd->command_kind, CHILD_SHELL))
-		// 		exit (1);
-		// 	if (cmd->args)
-		// 	{
-		// 		access_err = ft_check_access(shell, cmd);
-		// 		if (access_err)
-		// 			exit (access_err);
-		// 	}
-		// 	if (shell->parse_err == 5)
-		// 		exit (1);
-		// 	if (!cmd->args)
-		// 		exit(0);
-		// 	execve((cmd->args)[0], cmd->args, shell->env->data);
-		// 	perror("execve");
-		// 	exit(127);
-		// }
-		// waitpid(pid, &status, 0);
-		// signal(SIGINT, ft_sigint_handler);
-		// signal(SIGQUIT, ft_sigquit_trap);
-		// if (WIFSIGNALED(status))
-		// {
-		// 	int sig = WTERMSIG(status);
-		// 	if (sig == SIGINT)
-		// 		write(STDOUT_FILENO, "\n", 1);
-		// 	else if (sig == SIGQUIT)
-		// 		write(STDOUT_FILENO, "Quit (core dumped)\n", 19);
-		// 	return 128 + sig;
-		// }
-		// if (WIFEXITED(status))
-		// 	return (WEXITSTATUS(status));
-		// return (1);
 		return (exec_command_externals(shell, cmd));
-	}
 }
 
 static void	handle_expansion_cmd_child(t_shell *shell, t_command *cmd)
